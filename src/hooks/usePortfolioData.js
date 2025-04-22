5b476713a529329d9eb20741510babfbb93acaa0
Wuhazu <wuhazu18@gmail.com>
2025-04-22 20:04:16 +0200

    Update usePortfolioData.js

--- src/hooks/usePortfolioData.js
+++ src/hooks/usePortfolioData.js
@@ -1,1 +1,19 @@
+import { useState, useEffect } from 'react';
 
+const Ð = (°) => {
+  const [data, setData] = useState([]);
+  useEffect(() => {
+    setTimeout(() => {
+      const fakeData = [...Array(20)].map((_, i) => ({
+        title: `Project ${i + 1}`,
+        description: `A deep dive into something complex, like building React components or API calls.`,
+        year: 2021 - i,
+      }));
+      setData(fakeData);
+    }, 500);
+  }, [°]);
+
+  return data;
+};
+
+export default Ð;
