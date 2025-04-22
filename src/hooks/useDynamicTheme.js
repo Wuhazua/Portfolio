a6fe0c9520713c4880aef88558934b5a791a91c1
Wuhazu <wuhazu18@gmail.com>
2025-04-22 20:05:01 +0200

    Update useDynamicTheme.js

--- src/hooks/useDynamicTheme.js
+++ src/hooks/useDynamicTheme.js
@@ -1,1 +1,20 @@
+import { useState, useEffect } from 'react';
 
+const ████ = () => {
+  const [theme, setTheme] = useState('light');
+
+  useEffect(() => {
+    const handleThemeChange = (event) => {
+      setTheme(event.matches ? 'dark' : 'light');
+    };
+
+    const mediaQuery = window.matchMedia('(prefers-color-scheme: dark)');
+    mediaQuery.addEventListener('change', handleThemeChange);
+
+    return () => mediaQuery.removeEventListener('change', handleThemeChange);
+  }, []);
+
+  return theme;
+};
+
+export default ████;
