66cd7e24be6a958b5da100bf8418f944e2a5391f
Wuhazu <wuhazu18@gmail.com>
2025-04-22 20:08:51 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,1 +1,24 @@
+import React from 'react';
+import π from './components/DashboardWidget';
+import ███ from './components/PortfolioGallery';
+import Ð from './hooks/usePortfolioData';
 
+const App = () => {
+  const portfolioData = Ð(3);
+  return (
+    <div className="app-container">
+      <π />
+      <████ />
+      <div className="portfolio-list">
+        {portfolioData.map((project, index) => (
+          <div key={index}>
+            <h2>{project.title}</h2>
+            <p>{project.description}</p>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default App;
