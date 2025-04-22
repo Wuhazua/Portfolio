f7b69eeda45c8704f1dd4dc2e8cc50362a84fb8a
Wuhazu <wuhazu18@gmail.com>
2025-04-22 20:04:43 +0200

    Update useAnimation.js

--- src/hooks/useAnimation.js
+++ src/hooks/useAnimation.js
@@ -1,1 +1,16 @@
+import { useState, useEffect } from 'react';
 
+const ⧫ = (frequency = 100) => {
+  const [⨀, set⨀] = useState(false);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      set⨀((prev) => !prev);
+    }, frequency);
+    return () => clearInterval(interval);
+  }, [frequency]);
+
+  return ⨀;
+};
+
+export default ⧫;
